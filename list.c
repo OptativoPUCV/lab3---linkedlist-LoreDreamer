b875a2f7a02036b20a61abb6f4ed4ddd92223291
Your Name <you@example.com>
2024-03-27 17:08:35 +0000

    new try

--- list.c
+++ list.c
@@ -29,7 +29,14 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
-     return NULL;
+
+  List *lista = (List *) malloc(sizeof(List));
+  
+  lista->current = NULL;
+  lista->head = NULL;
+  lista->tail = NULL;
+  
+  return lista;
 }
 
 void * firstList(List * list) {
